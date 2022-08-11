38f63893b857e4e96e0bac2b788b3efa9f6d9d88
Alejandro Martinez <goatmix1@gmail.com>
2022-08-11 15:41:15 -0400

    added home.js, and home.css, some components for the home page(slider and carousel)

---
+++ client/src/assets/homeimages.js
@@ -0,0 +1,7 @@
+import image2 from "./fruitjuice2.jpg";
+import image3 from "./fruitjuice3.jpg";
+import image4 from "./fruitjuice4.jpg";
+import image5 from "./fruitjuice5.jpg";
+import image6 from "./fruitjuice6.avif";
+
+export default [image2, image3, image4, image5, image6];
