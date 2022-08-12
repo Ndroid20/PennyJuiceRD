24b4edd5c2cc77e69cf174254c2583486f3f0518
hyeb96 <eban.hdit@gmail.com>
2022-08-11 20:13:05 -0400

    test

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,17 @@
-import { Juice } from './Components/Juice';
+// import { Juice } from './Components/Juice';
 import './App.css';
 import Navbar from './Components/Navbar';
 import Footer from './Components/Footer';
 import About from './Components/About';
+import Test from './Components/Test.js'
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      {/* <Navbar /> */}
       {/* <Home /> */}
-      <About />
-
-      <Footer />
+      {/* <About /> */}
+      {/* <Footer /> */}
     </div>
   );
 }
